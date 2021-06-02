50401c9416cb5a2d2e6d1083235d87aa15588639
Daniel Liberty <daniel.liberty48@gmail.com>
2021-06-02 18:28:59 +0300

    rotating cube

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas, useFrame } from "react-three-fiber";
+import React, { useState, useRef } from "react";
+import { useSpring, a } from "@react-spring/three";
+
+const Boxa = () => {
+  const meshRef = useRef();
+  const [hovered, setHovered] = useState(false);
+  const props = useSpring({ color: hovered ? "hotpink" : "gray" });
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.01;
+    meshRef.current.rotation.x += 0.015;
+    meshRef.current.rotation.z += 0.007;
+  });
+
+  return (
+    <a.mesh
+      ref={meshRef}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[0.99, 0.99, 0.99]} />
+      <a.meshBasicMaterial
+        attach="material"
+        color={props.color}
+        wireframe={false}
+      />
+    </a.mesh>
+  );
+};
+
+const Box = () => {
+  const meshRef = useRef();
+  const [hovered, setHovered] = useState(false);
+  const props = useSpring({ color: hovered ? "hotpink" : "gray" });
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.01;
+    meshRef.current.rotation.x += 0.015;
+    meshRef.current.rotation.z += 0.007;
+  });
+
+  return (
+    <a.mesh
+      ref={meshRef}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <a.meshBasicMaterial attach="material" color="red" wireframe={true} />
+    </a.mesh>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas style={{ position: "absolute" }}>
+        <Box />
+        <Boxa />
+      </Canvas>
+    </>
   );
 }
 
